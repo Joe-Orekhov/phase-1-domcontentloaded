b201accba103983ea8c96e40f1ed2f717fb60719
joesephorekhov <josephorekhov@gmail.com>
2021-08-16 10:55:10 -0600

    commit tree 91e3e786f2cbcc76579b261704e56d02cc2316b5

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', function(){
+  const newPhrase = document.querySelector('p').innerHTML = 'This is really cool!';
+  return (newPhrase);
+})
+
